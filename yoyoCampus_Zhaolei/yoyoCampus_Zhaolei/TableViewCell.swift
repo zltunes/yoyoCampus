0187435bf98432a671d0cf128ee4f4c67d2b8c7c
Zhr <369919696@qq.com>
2015-11-19 20:56:50 +0800

    test ignore

---
+++ yoyoCampus_Zhaolei/yoyoCampus_Zhaolei/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  yoyoCampus_Zhaolei
+//
+//  Created by 浩然 on 15/11/19.
+//  Copyright © 2015年 赵磊. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
