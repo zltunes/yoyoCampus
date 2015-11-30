bc3487ea1002768914e69e507307681625921747
Zhr <369919696@qq.com>
2015-11-30 22:51:25 +0800

    add idle collection

--- yoyoCampus_Zhaolei/yoyoCampus_Zhaolei/MyFavoriteVC.swift
+++ yoyoCampus_Zhaolei/yoyoCampus_Zhaolei/MyFavoriteVC.swift
@@ -15,6 +15,8 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
     
     var resultGoodsArray = NSMutableArray()
     var resultShopArray = NSMutableArray()
+    var resultIdleArray = NSMutableArray()
+    var btnIdle = UIButton()
     var btnGoods = UIButton()
     var btnShop = UIButton()
     var rootView = UIScrollView()
@@ -22,12 +24,16 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
     var scrollIndicator = UIScrollView()
     var tableViewGoods = UITableView()
     var tableViewShop = UITableView()
+    var tableViewIdle = UITableView()
+    
     
     var view1 = UIView()
     var view2 = UIView()
+    var view3 = UIView()
     
     var pageGoods = 1
     var pageShop = 1
+    var pageIdle = 1
     
     var didReceiveData = false
 
@@ -37,6 +43,9 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
         }
         if(self.pageView.currentPage == 1){
             self.pageTurn(self.btnShop)
+        }
+        if(self.pageView.currentPage == 2){
+            self.pageTurn(self.btnIdle)
         }
         self.scrollIndicator.contentOffset.x = -(CGFloat(self.pageView.currentPage) * windowWidth/2)
     }
@@ -76,9 +85,9 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
         
         let scrollIndicator = UIScrollView()
         let indicatorView = UIView()
-        scrollIndicator.frame = CGRectMake(60, 40, windowWidth-100, 3)
+        scrollIndicator.frame = CGRectMake(windowWidth*0.1, 40, windowWidth*0.8, 3)
         scrollIndicator.contentSize = CGSize(width: windowWidth-160, height: 3)
-        indicatorView.frame = CGRect(x: 0, y: 0, width: 80, height: 3)
+        indicatorView.frame = CGRect(x: 0, y: 0, width: windowWidth*0.4/3, height: 3)
         indicatorView.backgroundColor = UIColor(red: 73/255, green: 185/255, blue: 162/255, alpha: 1)
         scrollIndicator.addSubview(indicatorView)
         self.view.addSubview(scrollIndicator)
@@ -86,7 +95,7 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
         
         
         
-        let btnGoods = UIButton(frame: CGRectMake(0, 0, windowWidth/2, 40))
+        let btnGoods = UIButton(frame: CGRectMake(0, 0, windowWidth/3, 40))
         btnGoods.tag = 0
         btnGoods.setTitle("商品", forState: UIControlState.Normal)
         btnGoods.addTarget(self, action: Selector("pageTurn:"), forControlEvents: UIControlEvents.TouchUpInside)
@@ -96,7 +105,7 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
         self.view.addSubview(btnGoods)
         self.btnGoods = btnGoods
         
-        let btnShop = UIButton(frame: CGRectMake(windowWidth/2, 0, windowWidth/2, 40))
+        let btnShop = UIButton(frame: CGRectMake(windowWidth/3, 0, windowWidth/3, 40))
         btnShop.tag = 1
         btnShop.setTitle("商家", forState: UIControlState.Normal)
         btnShop.addTarget(self, action: Selector("pageTurn:"), forControlEvents: UIControlEvents.TouchUpInside)
@@ -106,6 +115,17 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
         self.view.addSubview(btnShop)
         self.btnShop = btnShop
         
+        let btnIdle = UIButton(frame: CGRectMake(windowWidth/3*2, 0, windowWidth/3, 40))
+        btnIdle.tag = 2
+        btnIdle.setTitle("闲置", forState: UIControlState.Normal)
+        btnIdle.addTarget(self, action: Selector("pageTurn:"), forControlEvents: UIControlEvents.TouchUpInside)
+        btnIdle.backgroundColor = UIColor.whiteColor()
+        btnIdle.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+        btnIdle.titleLabel?.font = UIFont(name: "Verdana", size: 13)
+        self.view.addSubview(btnIdle)
+        self.btnIdle = btnIdle
+        
+        
         let view1 = UIView(frame: CGRectMake(0, 0, windowWidth, windowHeight))
         self.rootView.addSubview(view1)
         self.view1 = view1
@@ -114,7 +134,11 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
         self.rootView.addSubview(view2)
         self.view2 = view2
         
-        var tableViewGoods = UITableView(frame: CGRectMake(0, 45, windowWidth, windowHeight))
+        let view3 = UIView(frame: CGRectMake(windowWidth*2, 0, windowWidth, windowHeight))
+        self.rootView.addSubview(view3)
+        self.view3 = view3
+        
+        let tableViewGoods = UITableView(frame: CGRectMake(0, 45, windowWidth, windowHeight))
         view1.addSubview(tableViewGoods)
         self.setExtralCellLinesHidden(tableViewGoods)
         tableViewGoods.rowHeight = windowHeight/5
@@ -126,7 +150,7 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
         tableViewGoods.mj_footer.tag = 0
         self.tableViewGoods = tableViewGoods
         
-        var tableViewShop = UITableView(frame: CGRectMake(0, 45, windowWidth, windowHeight))
+        let tableViewShop = UITableView(frame: CGRectMake(0, 45, windowWidth, windowHeight))
         self.setExtralCellLinesHidden(tableViewShop)
         tableViewShop.rowHeight = windowHeight/5
         tableViewShop.dataSource = self
@@ -137,6 +161,18 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
         tableViewShop.mj_footer.tag = 1
         view2.addSubview(tableViewShop)
         self.tableViewShop = tableViewShop
+        
+        let tableViewIdle = UITableView(frame: CGRectMake(0, 45, windowWidth, windowHeight))
+        self.setExtralCellLinesHidden(tableViewIdle)
+        tableViewIdle.rowHeight = windowHeight/5
+        tableViewIdle.dataSource = self
+        tableViewIdle.delegate = self
+        tableViewIdle.mj_header = MJRefreshNormalHeader(refreshingTarget: self, refreshingAction: Selector("headerRefreshing:"))
+        tableViewIdle.mj_header.tag = 2
+        tableViewIdle.mj_footer = MJRefreshAutoNormalFooter(refreshingTarget: self, refreshingAction: Selector("footerRefreshing:"))
+        tableViewIdle.mj_footer.tag = 2
+        view3.addSubview(tableViewIdle)
+        self.tableViewIdle = tableViewIdle
     }
     
     func httpGetData(){
@@ -155,11 +191,22 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
             let json = JSON(response.result.value!)
             var responseJson = json["collection"]
             self.resultShopArray = NSMutableArray(array: responseJson.arrayObject!)
+            self.GetIdleData()
+        }
+    }
+    
+    func GetIdleData(){
+        Alamofire.request(.GET, "http://api2.hloli.me:9001/v1.0/idle/collection/1",headers:httpHeader).responseJSON(options: NSJSONReadingOptions.MutableContainers){
+            response in
+            let json = JSON(response.result.value!)
+            var responseJson = json["collection"]
+            self.resultIdleArray = NSMutableArray(array: responseJson.arrayObject!)
             self.didReceiveData = true
             self.setAllView()
             self.btnColorChange(0)
             self.tableViewGoods.reloadData()
             self.tableViewShop.reloadData()
+            self.tableViewIdle.reloadData()
         }
     }
     
@@ -170,15 +217,22 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
         }
         if(self.didReceiveData == true){
             if(tableView == self.tableViewGoods){
-                var cell = ViewCell()
+                let cell = ViewCell()
                 cell.isIdleCell = false
                 cell.setData(self.resultGoodsArray[indexPath.row])
                 cell.selectionStyle = UITableViewCellSelectionStyle.None
                 return cell
             }
             if(tableView == self.tableViewShop){
-                var cell = ShopCell()
+                let cell = ShopCell()
                 cell.setData(self.resultShopArray[indexPath.row])
+                cell.selectionStyle = UITableViewCellSelectionStyle.None
+                return cell
+            }
+            if(tableView == self.tableViewIdle){
+                let cell = ViewCell()
+                cell.isIdleCell = true
+                cell.setData(self.resultIdleArray[indexPath.row])
                 cell.selectionStyle = UITableViewCellSelectionStyle.None
                 return cell
             }
@@ -198,12 +252,14 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
             if(tableView == self.tableViewShop){
                 return self.resultShopArray.count
             }
+            if(tableView == self.tableViewIdle){
+                return self.resultIdleArray.count
+            }
         }
         return 0
     }
     
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        print("1")
         if(tableView == self.tableViewGoods){
             let tempCell = tableView.cellForRowAtIndexPath(indexPath)as! ViewCell
             let goodsId : String = tempCell.dataCell.objectForKey("goods_id")as! String
@@ -220,6 +276,15 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
                 response in
                 self.resultShopArray.removeObjectAtIndex(indexPath.row)
                 self.tableViewShop.reloadData()
+            }
+        }
+        if(tableView == self.tableViewIdle){
+            let tempCell = tableView.cellForRowAtIndexPath(indexPath)as! ViewCell
+            let idleID : String = tempCell.dataCell.objectForKey("idle_id")as! String
+            Alamofire.request(.DELETE, "http://api2.hloli.me:9001/v1.0/idle/collection/" + idleID,headers:httpHeader).responseJSON(){
+                response in
+                self.resultIdleArray.removeObjectAtIndex(indexPath.row)
+                self.tableViewIdle.reloadData()
             }
         }
         
@@ -241,14 +306,20 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
             vc.shopID = tempCell.shopId
             self.hidesBottomBarWhenPushed = true
             self.navigationController?.pushViewController(vc, animated: true)
-            
+        }
+        if(tableView == self.tableViewIdle){
+            let tempCell = tableView.cellForRowAtIndexPath(indexPath)as! ViewCell
+            let vc = IdleGoodViewController()
+            vc.idle_id = tempCell.dataCell.objectForKey("idle_id")as!String
+            self.hidesBottomBarWhenPushed = true
+            self.navigationController?.pushViewController(vc, animated: true)
         }
     }
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
         if(scrollView == self.rootView){
             let offset : CGPoint = scrollView.contentOffset
-            self.scrollIndicator.contentOffset = CGPoint(x: -offset.x / 2, y: offset.y)
+            self.scrollIndicator.contentOffset = CGPoint(x: -offset.x / 3, y: offset.y)
         }
     }
     func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
@@ -269,6 +340,11 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
             self.tableViewShop.reloadData()
             self.btnColorChange(1)
         }
+        if(sender.tag == 2){
+            self.tableViewIdle.reloadData()
+            self.btnColorChange(2)
+        }
+        
     }
     func scrollPageTurn(sender : Int){
         if(sender == 0){
@@ -279,15 +355,26 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
             self.tableViewShop.reloadData()
             self.btnColorChange(1)
         }
+        if(sender == 2){
+            self.tableViewIdle.reloadData()
+            self.btnColorChange(2)
+        }
     }
     func btnColorChange(sender : Int){
         if(sender == 0){
             self.btnGoods.setTitleColor(UIColor(red: 73/255, green: 185/255, blue: 162/255, alpha: 1), forState: UIControlState.Normal)
             self.btnShop.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+            self.btnIdle.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
         }
         if(sender == 1){
             self.btnShop.setTitleColor(UIColor(red: 73/255, green: 185/255, blue: 162/255, alpha: 1), forState: UIControlState.Normal)
             self.btnGoods.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+            self.btnIdle.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+        }
+        if(sender == 2){
+            self.btnShop.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+            self.btnGoods.setTitleColor(UIColor.grayColor(), forState: UIControlState.Normal)
+            self.btnIdle.setTitleColor(UIColor(red: 73/255, green: 185/255, blue: 162/255, alpha: 1), forState: UIControlState.Normal)
         }
     }
     
@@ -310,6 +397,16 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
                 self.resultShopArray = NSMutableArray(array: responseJson.arrayObject!)
                 self.tableViewShop.reloadData()
                 self.tableViewShop.mj_header!.endRefreshing()
+            }
+        }
+        if(sender.tag == 2){
+            Alamofire.request(.GET, "http://api2.hloli.me:9001/v1.0/idle/collection/1",headers:httpHeader).responseJSON(options: NSJSONReadingOptions.MutableContainers){
+                response in
+                let json = JSON(response.result.value!)
+                var responseJson = json["collection"]
+                self.resultIdleArray = NSMutableArray(array: responseJson.arrayObject!)
+                self.tableViewIdle.reloadData()
+                self.tableViewIdle.mj_header!.endRefreshing()
             }
         }
     }
@@ -350,6 +447,25 @@ class MyFavoriteVC: UIViewController,UIScrollViewDelegate ,UITableViewDataSource
                     }
                     self.tableViewShop.reloadData()
                     self.tableViewShop.mj_footer!.endRefreshing()
+                }
+            }
+        }
+        if(sender.tag == 2){
+            self.pageIdle++
+            Alamofire.request(.GET, "http://api2.hloli.me:9001/v1.0/idle/collection/" + String(self.pageIdle),headers:httpHeader).responseJSON(options: NSJSONReadingOptions.MutableContainers){
+                response in
+                let json = JSON(response.result.value!)
+                var responseJson = json["collection"]
+                if(responseJson.count == 0){
+                    self.pageShop--
+                    self.tableViewIdle.mj_footer!.endRefreshing()
+                }
+                if(responseJson.count != 0){
+                    for(var num = 0 ; num < responseJson.count ; num++){
+                        self.resultIdleArray.addObject(responseJson.arrayObject![num])
+                    }
+                    self.tableViewIdle.reloadData()
+                    self.tableViewIdle.mj_footer!.endRefreshing()
                 }
             }
         }
